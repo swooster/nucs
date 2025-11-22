b9265ca86fee2d7da6a57701b7aa3c89f7858959
Steve Wooster <s.f.m.wooster@gmail.com>
2025-11-22 08:18:44 -0800

    Speed up `rand` impls.

--- src/rand.rs
+++ src/rand.rs
@@ -2,27 +2,41 @@
 
 use rand::Rng;
 use rand::distr::{Distribution, StandardUniform};
-use rand::seq::IndexedRandom;
 
 use crate::{AmbiAmino, AmbiNuc, Amino, Nuc};
 
+// NOTE: The disperate styles of random generation are a result of what I've found to be
+// fastest for each type. Also note that the size of the `expect` messages seems to influence
+// speed even though it appears to be dead code..? ¯\_(ツ)_/¯
+
 impl Distribution<Nuc> for StandardUniform {
     fn sample<R: Rng + ?Sized>(&self, rng: &mut R) -> Nuc {
-        *(Nuc::ALL).choose(rng).expect("BUG: Nuc::ALL was empty")
+        let num_nucs = u8::try_from(Nuc::ALL.len()).expect("Nuc should be repr(u8)");
+        let index: u8 = rng.random_range(0..num_nucs);
+        match index {
+            0 => Nuc::A,
+            1 => Nuc::C,
+            2 => Nuc::G,
+            3 => Nuc::T,
+            _ => unreachable!("BUG: bad RNG Nuc"),
+        }
     }
 }
 
 impl Distribution<AmbiNuc> for StandardUniform {
     fn sample<R: Rng + ?Sized>(&self, rng: &mut R) -> AmbiNuc {
-        *(AmbiNuc::ALL)
-            .choose(rng)
-            .expect("BUG: AmbiNuc::ALL was empty")
+        let num_ambi_nucs = u8::try_from(AmbiNuc::ALL.len()).expect("AmbiNuc should be repr(u8)");
+        #[allow(clippy::range_plus_one, reason = "this would halve speed")]
+        let index: u8 = rng.random_range(1..(num_ambi_nucs + 1));
+        AmbiNuc::from_u8(index).expect("BUG: bad RNG AmbiNuc")
     }
 }
 
 impl Distribution<Amino> for StandardUniform {
     fn sample<R: Rng + ?Sized>(&self, rng: &mut R) -> Amino {
-        *(Amino::ALL).choose(rng).expect("BUG: Amino::ALL was empty")
+        let num_aminos = u8::try_from(Amino::ALL.len()).expect("Amino should be repr(u8)");
+        let index: u8 = rng.random_range(0..num_aminos);
+        Amino::ALL[index as usize]
     }
 }
 
