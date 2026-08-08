2dce9d057b57f40a0314c14aeab4b2cd2031c5b6
Steve Wooster <s.f.m.wooster@gmail.com>
2026-08-08 04:10:03 -0700

    Add minimal packed types.
    This is missing lots of important features, but it at least enables less
    memory use, both for `Vec`s and arrays, for all `Symbol` types.

---
+++ src/packed/ambidna.rs
@@ -0,0 +1,201 @@
+use crate::AmbiNuc;
+
+use super::{ArrayDefault, ArrayDivide};
+
+// Note on storage: AmbiNucs are packed big-endian so naive lexical sorting of bytes is correct.
+//
+// The last byte of a non-empty `PackedAmbiDna` must have 1-2 elements. If it has 1 element,
+// the least-significant 4 bits are b0000, which isn't a valid representation.
+
+/// Like [`Vec<AmbiNuc>`], but takes 50% less space for long DNA sequences.
+///
+/// # Examples
+///
+/// ```
+/// use nucs::{AmbiNuc, Packed};
+///
+/// let dna = AmbiNuc::arr(b"ACATATTACK");
+/// let packed: Packed<[AmbiNuc]> = dna.as_slice().into();
+/// let unpacked: Vec<AmbiNuc> = packed.into();
+/// assert_eq!(unpacked, dna);
+/// ```
+#[derive(Clone)]
+pub struct PackedAmbiDna(Vec<u8>);
+
+impl From<&[AmbiNuc]> for PackedAmbiDna {
+    fn from(ambi_nucs: &[AmbiNuc]) -> PackedAmbiDna {
+        let packed_len = ambi_nucs.len().div_ceil(2);
+        let mut packed = vec![0; packed_len];
+        pack(&mut packed, ambi_nucs);
+        Self(packed)
+    }
+}
+
+impl From<PackedAmbiDna> for Vec<AmbiNuc> {
+    fn from(packed_ambi_nucs: PackedAmbiDna) -> Vec<AmbiNuc> {
+        (&packed_ambi_nucs).into()
+    }
+}
+
+impl From<&PackedAmbiDna> for Vec<AmbiNuc> {
+    fn from(packed_ambi_nucs: &PackedAmbiDna) -> Vec<AmbiNuc> {
+        if let Some(byte) = packed_ambi_nucs.0.last() {
+            let len = 2 * packed_ambi_nucs.0.len() - usize::from(byte.trailing_zeros() >= 4);
+            let mut ambi_nucs = vec![AmbiNuc::default(); len];
+            unpack(&mut ambi_nucs, &packed_ambi_nucs.0);
+            ambi_nucs
+        } else {
+            vec![]
+        }
+    }
+}
+
+/// Like [`[AmbiNuc; N]`](array), but takes 50% less space.
+///
+/// # Examples
+///
+/// ```
+/// use nucs::{AmbiNuc, Packed};
+///
+/// let dna = AmbiNuc::arr(b"ACATATTACK");
+/// assert_eq!(size_of_val(&dna), 10);
+/// let packed: Packed<[AmbiNuc; 10]> = dna.into();
+/// assert_eq!(size_of_val(&packed), 5);
+/// let unpacked: [AmbiNuc; 10] = packed.into();
+/// assert_eq!(unpacked, dna);
+/// ```
+#[derive(Clone, Copy)]
+pub struct PackedArrayAmbiDna<const N: usize>(<[(); N] as ArrayDivide>::By2<u8>)
+where
+    [(); N]: ArrayDivide;
+
+impl<const N: usize> From<[AmbiNuc; N]> for PackedArrayAmbiDna<N>
+where
+    [(); N]: ArrayDivide,
+{
+    fn from(ambi_nucs: [AmbiNuc; N]) -> PackedArrayAmbiDna<N> {
+        (&ambi_nucs).into()
+    }
+}
+
+impl<const N: usize> From<&[AmbiNuc; N]> for PackedArrayAmbiDna<N>
+where
+    [(); N]: ArrayDivide,
+{
+    fn from(ambi_nucs: &[AmbiNuc; N]) -> PackedArrayAmbiDna<N> {
+        let mut this = Self(ArrayDefault::array_default());
+        pack(this.0.as_mut(), ambi_nucs);
+        this
+    }
+}
+
+impl<const N: usize> From<PackedArrayAmbiDna<N>> for [AmbiNuc; N]
+where
+    [(); N]: ArrayDivide,
+{
+    fn from(packed_ambi_nucs: PackedArrayAmbiDna<N>) -> [AmbiNuc; N] {
+        (&packed_ambi_nucs).into()
+    }
+}
+
+impl<const N: usize> From<&PackedArrayAmbiDna<N>> for [AmbiNuc; N]
+where
+    [(); N]: ArrayDivide,
+{
+    fn from(packed_ambi_nucs: &PackedArrayAmbiDna<N>) -> [AmbiNuc; N] {
+        let mut ambi_nucs = [AmbiNuc::default(); N];
+        unpack(&mut ambi_nucs, packed_ambi_nucs.0.as_ref());
+        ambi_nucs
+    }
+}
+
+fn pack(packed: &mut [u8], ambi_nucs: &[AmbiNuc]) {
+    let (pairs, remainder) = ambi_nucs.as_chunks();
+    for (byte, &[n1, n2]) in packed.iter_mut().zip(pairs) {
+        *byte = n2.compress() | (n1.compress() << 4);
+    }
+    if let Some(byte) = packed.last_mut()
+        && let [ambi_nuc] = remainder
+    {
+        *byte = ambi_nuc.compress() << 4;
+    }
+}
+
+fn unpack(ambi_nucs: &mut [AmbiNuc], packed: &[u8]) {
+    let (pairs, remainder) = ambi_nucs.as_chunks_mut();
+    for ([n1, n2], &byte) in pairs.iter_mut().zip(packed) {
+        *n1 = AmbiNuc::decompress(byte >> 4);
+        *n2 = AmbiNuc::decompress(byte);
+    }
+    if let Some(byte) = packed.last()
+        && let [ambi_nuc] = remainder
+    {
+        *ambi_nuc = AmbiNuc::decompress(byte >> 4);
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use proptest::{arbitrary::any, proptest};
+
+    use super::super::tests::{assert_both_roundtrips, assert_roundtrip};
+    use crate::proptest::any_ambi_dna;
+
+    use super::*;
+
+    #[test]
+    fn all_short_roundtrips() {
+        // Yes, this is hideously ugly, but arrays need a size known at compile-time,
+        // and this is the simplest way to ensure I get them all.
+        assert_both_roundtrips(&[] as &[AmbiNuc; 0]);
+        for n1 in AmbiNuc::ALL {
+            assert_both_roundtrips(&[n1]);
+            for n2 in AmbiNuc::ALL {
+                assert_both_roundtrips(&[n1, n2]);
+                for n3 in AmbiNuc::ALL {
+                    assert_both_roundtrips(&[n1, n2, n3]);
+                    for n4 in AmbiNuc::ALL {
+                        assert_both_roundtrips(&[n1, n2, n3, n4]);
+                    }
+                }
+            }
+        }
+    }
+
+    proptest! {
+        #[cfg_attr(miri, ignore = "slow in miri; shouldn't touch unsafe code anyway")]
+        #[test]
+        fn packed_ambi_dna_roundtrip(
+            ambi_dna in any_ambi_dna(5..25) // 0..=4 covered above
+        ) {
+            assert_roundtrip(&*ambi_dna);
+        }
+
+        #[cfg_attr(miri, ignore = "slow in miri; shouldn't touch unsafe code anyway")]
+        #[test]
+        fn packed_array_ambi_dna5_roundtrip(
+            ambi_dna in any::<[AmbiNuc; 5]>()
+        ) {
+            assert_roundtrip(&ambi_dna);
+        }
+
+        #[cfg_attr(miri, ignore = "slow in miri; shouldn't touch unsafe code anyway")]
+        #[test]
+        fn packed_array_ambi_dna6_roundtrip(
+            ambi_dna in any::<[AmbiNuc; 6]>()
+        ) {
+            assert_roundtrip(&ambi_dna);
+        }
+
+        #[cfg_attr(miri, ignore = "slow in miri; shouldn't touch unsafe code anyway")]
+        #[test]
+        fn packed_ambi_dna_lexical_ordering(
+            dna1 in any_ambi_dna(0..50),
+            dna2 in any_ambi_dna(0..50),
+        ) {
+            let packed1 = PackedAmbiDna::from(dna1.as_slice());
+            let packed2 = PackedAmbiDna::from(dna2.as_slice());
+            assert_eq!(packed1.0.cmp(&packed2.0), dna1.cmp(&dna2));
+        }
+    }
+}
---
+++ src/packed/dna.rs
@@ -0,0 +1,238 @@
+use crate::Nuc;
+
+use super::{ArrayDefault, ArrayDivide};
+
+// Note on storage: Nucs are packed big-endian. Sadly, this encoding DOES NOT maintain lexical
+// ordering, due to the suffix (see below).
+//
+// The last byte of a non-empty `PackedDna` must have 0-3 elements. Any missing element is encoded
+// 0b00, and the the least significant 2 bits record the number of elements.
+//
+// We do this rather than store the length externally, both to minimize the memory usage
+// (with inline lengths, you don't need any extra memory 75% of the time, whereas with an
+// external u8, the Vec overhead would increase from 24 to 32 bytes) and to ensure the
+// length can be correctly inferred from serialized data.
+
+/// Like [`Vec<Nuc>`], but takes 75% less space for long DNA sequences.
+///
+/// # Examples
+///
+/// ```
+/// use nucs::{Nuc, Packed};
+///
+/// let dna = Nuc::arr(b"ACATATTAC");
+/// let packed: Packed<[Nuc]> = dna.as_slice().into();
+/// let unpacked: Vec<Nuc> = packed.into();
+/// assert_eq!(unpacked, dna);
+/// ```
+#[derive(Clone)]
+pub struct PackedDna(Vec<u8>);
+
+impl From<&[Nuc]> for PackedDna {
+    fn from(nucs: &[Nuc]) -> PackedDna {
+        let packed_len = match nucs.len() {
+            0 => 0, // special-case to reduce allocs
+            l => l / 4 + 1,
+        };
+        let mut packed = vec![0; packed_len];
+        pack(&mut packed, nucs);
+        if let Some(byte) = packed.last_mut() {
+            *byte |=
+                u8::try_from(nucs.len() % 4).unwrap_or_else(|_| unreachable!("x % 4 fits in a u8"));
+        }
+        Self(packed)
+    }
+}
+
+impl From<PackedDna> for Vec<Nuc> {
+    fn from(packed_nucs: PackedDna) -> Vec<Nuc> {
+        (&packed_nucs).into()
+    }
+}
+
+impl From<&PackedDna> for Vec<Nuc> {
+    fn from(packed_nucs: &PackedDna) -> Vec<Nuc> {
+        if let Some(byte) = packed_nucs.0.last() {
+            let len = 4 * (packed_nucs.0.len() - 1) + (byte & 0b11) as usize;
+            let mut nucs = vec![Nuc::default(); len];
+            unpack(&mut nucs, &packed_nucs.0);
+            nucs
+        } else {
+            vec![]
+        }
+    }
+}
+
+/// Like [`[Nuc; N]`](array), but takes 75% less space.
+///
+/// # Examples
+///
+/// ```
+/// use nucs::{Nuc, Packed};
+///
+/// let dna = Nuc::arr(b"ACATATTAC");
+/// assert_eq!(size_of_val(&dna), 9);
+/// let packed: Packed<[Nuc; 9]> = dna.into();
+/// assert_eq!(size_of_val(&packed), 3);
+/// let unpacked: [Nuc; 9] = packed.into();
+/// assert_eq!(unpacked, dna);
+/// ```
+#[derive(Clone, Copy)]
+pub struct PackedArrayDna<const N: usize>(<[(); N] as ArrayDivide>::By4<u8>)
+where
+    [(); N]: ArrayDivide;
+
+impl<const N: usize> From<[Nuc; N]> for PackedArrayDna<N>
+where
+    [(); N]: ArrayDivide,
+{
+    fn from(nucs: [Nuc; N]) -> PackedArrayDna<N> {
+        (&nucs).into()
+    }
+}
+
+impl<const N: usize> From<&[Nuc; N]> for PackedArrayDna<N>
+where
+    [(); N]: ArrayDivide,
+{
+    fn from(nucs: &[Nuc; N]) -> PackedArrayDna<N> {
+        let mut this = Self(ArrayDefault::array_default());
+        pack(this.0.as_mut(), nucs);
+        this
+    }
+}
+
+impl<const N: usize> From<PackedArrayDna<N>> for [Nuc; N]
+where
+    [(); N]: ArrayDivide,
+{
+    fn from(packed_nucs: PackedArrayDna<N>) -> [Nuc; N] {
+        (&packed_nucs).into()
+    }
+}
+
+impl<const N: usize> From<&PackedArrayDna<N>> for [Nuc; N]
+where
+    [(); N]: ArrayDivide,
+{
+    fn from(packed_nucs: &PackedArrayDna<N>) -> [Nuc; N] {
+        let mut nucs = [Nuc::default(); N];
+        unpack(&mut nucs, packed_nucs.0.as_ref());
+        nucs
+    }
+}
+
+fn pack(packed: &mut [u8], nucs: &[Nuc]) {
+    let (quads, remainder) = nucs.as_chunks();
+    for (byte, &[n1, n2, n3, n4]) in packed.iter_mut().zip(quads) {
+        *byte = n4.compress() | (n3.compress() << 2) | (n2.compress() << 4) | (n1.compress() << 6);
+    }
+    if let Some(byte) = packed.last_mut()
+        && !remainder.is_empty()
+    {
+        *byte = 0;
+        for (offset, &nuc) in [6, 4, 2].into_iter().zip(remainder) {
+            *byte |= nuc.compress() << offset;
+        }
+    }
+}
+
+fn unpack(nucs: &mut [Nuc], packed: &[u8]) {
+    let (quads, remainder) = nucs.as_chunks_mut();
+    for ([n1, n2, n3, n4], &byte) in quads.iter_mut().zip(packed) {
+        *n4 = Nuc::decompress(byte);
+        *n3 = Nuc::decompress(byte >> 2);
+        *n2 = Nuc::decompress(byte >> 4);
+        *n1 = Nuc::decompress(byte >> 6);
+    }
+    if let Some(byte) = packed.last()
+        && !remainder.is_empty()
+    {
+        for (offset, nuc) in [6, 4, 2].into_iter().zip(remainder) {
+            *nuc = Nuc::decompress(byte >> offset);
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use proptest::{arbitrary::any, proptest};
+
+    use super::super::tests::{assert_both_roundtrips, assert_roundtrip};
+    use crate::proptest::any_dna;
+
+    use super::*;
+
+    #[test]
+    fn all_short_roundtrips() {
+        // Yes, this is hideously ugly, but arrays need a size known at compile-time,
+        // and this is the simplest way to ensure I get them all.
+        assert_both_roundtrips(&[] as &[Nuc; 0]);
+        for n1 in Nuc::ALL {
+            assert_both_roundtrips(&[n1]);
+            for n2 in Nuc::ALL {
+                assert_both_roundtrips(&[n1, n2]);
+                for n3 in Nuc::ALL {
+                    assert_both_roundtrips(&[n1, n2, n3]);
+                    for n4 in Nuc::ALL {
+                        assert_both_roundtrips(&[n1, n2, n3, n4]);
+                        for n5 in Nuc::ALL {
+                            assert_both_roundtrips(&[n1, n2, n3, n4, n5]);
+                            for n6 in Nuc::ALL {
+                                assert_both_roundtrips(&[n1, n2, n3, n4, n5, n6]);
+                                for n7 in Nuc::ALL {
+                                    assert_both_roundtrips(&[n1, n2, n3, n4, n5, n6, n7]);
+                                    for n8 in Nuc::ALL {
+                                        assert_both_roundtrips(&[n1, n2, n3, n4, n5, n6, n7, n8]);
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    proptest! {
+        #[cfg_attr(miri, ignore = "slow in miri; shouldn't touch unsafe code anyway")]
+        #[test]
+        fn packed_dna_roundtrip(
+            dna in any_dna(9..50) // 0..=8 covered above
+        ) {
+            assert_roundtrip(&*dna);
+        }
+
+        #[cfg_attr(miri, ignore = "slow in miri; shouldn't touch unsafe code anyway")]
+        #[test]
+        fn packed_array_dna9_roundtrip(
+            dna in any::<[Nuc; 9]>()
+        ) {
+            assert_roundtrip(&dna);
+        }
+
+        #[cfg_attr(miri, ignore = "slow in miri; shouldn't touch unsafe code anyway")]
+        #[test]
+        fn packed_array_dna10_roundtrip(
+            dna in any::<[Nuc; 10]>()
+        ) {
+            assert_roundtrip(&dna);
+        }
+
+        #[cfg_attr(miri, ignore = "slow in miri; shouldn't touch unsafe code anyway")]
+        #[test]
+        fn packed_array_dna11_roundtrip(
+            dna in any::<[Nuc; 11]>()
+        ) {
+            assert_roundtrip(&dna);
+        }
+
+        #[cfg_attr(miri, ignore = "slow in miri; shouldn't touch unsafe code anyway")]
+        #[test]
+        fn packed_array_dna12_roundtrip(
+            dna in any::<[Nuc; 12]>()
+        ) {
+            assert_roundtrip(&dna);
+        }
+    }
+}
